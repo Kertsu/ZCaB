86b965f5c7a903f2e48de52e26f7dfeeed250743
Kertsu <kurtddbigtas@gmail.com>
2023-11-06 22:24:58 +0800

    added group gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -7,10 +7,18 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
  imageFilePaths: String[] = [];
+ imageFilePathsGroup: String[][] = [[], [], []];
 
  constructor() {
-  for (let i = 1; i<=9; i++){
-    this.imageFilePaths.push(`/assets/images/gallery/gallery_${i}.jpg`)
+  for (let i = 1; i<=15; i++){
+    this.imageFilePaths.push(`/assets/images/gallery_${i}.jpg`)
   }
+  for (let j = 1; j <= 15; j++) {
+    const division = j % 3;
+    
+    this.imageFilePathsGroup[division].push(`/assets/images/gallery_${j}.jpg`);
+}
+  
+
  }
 }
