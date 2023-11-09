92a78a718c610e7609af86f2984c4150ae0f8989
Kertsu <kurtddbigtas@gmail.com>
2023-11-09 14:38:22 +0800

    added 2 product pages

--- src/app/components/beddings/beddings.component.ts
+++ src/app/components/beddings/beddings.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./beddings.component.css']
 })
 export class BeddingsComponent {
+  imageFilePathsGroup: String[][] = [[], [], [], [], [], [], [], [], []];
 
+  constructor(){
+    for (let j = 1; j <= 18; j++) {
+      const division = j % 9;
+      
+      this.imageFilePathsGroup[division].push(`/assets/images/gallery/bedding_${j}.jpg`);
+  }
+  }
 }
--- src/app/components/curtains/curtains.component.ts
+++ src/app/components/curtains/curtains.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./curtains.component.css']
 })
 export class CurtainsComponent {
+  imageFilePathsGroup: String[][] = [[], [], [], [], [], [], [], [], []];
 
+  constructor(){
+    for (let j = 1; j <= 18; j++) {
+      const division = j % 9;
+      
+      this.imageFilePathsGroup[division].push(`/assets/images/gallery/curtain_${j}.jpg`);
+  }
+  }
 }
